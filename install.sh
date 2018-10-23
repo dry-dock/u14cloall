19837448c2b5867aa32915e2f9218175d47fea46
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-10-23 21:22:40 +0530

    update the java certificates

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo mkdir -p /var/lib/apt/lists/partial
 sudo apt-get clean
 sudo apt-get update
 
+/usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
+/var/lib/dpkg/info/ca-certificates-java.postinst configure
+
 # install Clojure's build tool: leiningen
 echo "================= Install clojure's build tool: leiningen ==================="
 sudo wget -nv https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
