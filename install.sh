a0c20ef203ab4daf3030222601b120f0815d4e24
vijayreddy1991 <31643910+vijayreddy1991@users.noreply.github.com>
2018-10-25 18:09:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ sudo mv /var/lib/apt/lists/* /tmp
 sudo mkdir -p /var/lib/apt/lists/partial
 sudo apt-get clean
 sudo apt-get update
-
-/usr/bin/printf '\xfe\xed\xfe\xed\x00\x00\x00\x02\x00\x00\x00\x00\xe2\x68\x6e\x45\xfb\x43\xdf\xa4\xd9\x92\xdd\x41\xce\xb6\xb2\x1c\x63\x30\xd7\x92' > /etc/ssl/certs/java/cacerts
-/var/lib/dpkg/info/ca-certificates-java.postinst configure
+sed -i 's/keystore.type=pkcs12/keystore.type=jks/g' /etc/java-11-openjdk/security/java.security
 
 # install Clojure's build tool: leiningen
 echo "================= Install clojure's build tool: leiningen ==================="
